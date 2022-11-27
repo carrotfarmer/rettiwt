db319855458853c27a7254d6db28dccb95da1204
Dhruva Srinivas <dhruvas17068@gmail.com>
2022-11-27 21:57:23 +0530

    feat: test like mutation

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,6 +8,7 @@ import { trpc } from "../utils/trpc";
 
 export const Home = () => {
   const { mutate: createTweet } = trpc.tweet.createTweet.useMutation();
+  const { mutate: likeTweet } = trpc.tweet.likeTweet.useMutation();
 
   return (
     <Box>
@@ -24,6 +25,15 @@ export const Home = () => {
             onClick={() => createTweet({ msg: "test" })}
           >
             create tweet
+          </Button>
+          <Button
+            colorScheme="twitter"
+            width="sm"
+            onClick={() =>
+              likeTweet({ tweetId: "cf193624-c424-430c-9b9e-0aa93b577fc7" })
+            }
+          >
+            like tweet
           </Button>
         </Center>
       </Box>
