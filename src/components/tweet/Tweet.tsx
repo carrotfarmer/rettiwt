144199e5843e25b605313d6eafc6fbbcf0b03dec
Dhruva Srinivas <dhruvas17068@gmail.com>
2022-12-28 18:25:40 +0530

    feat: tweet modal

--- src/components/tweet/Tweet.tsx
+++ src/components/tweet/Tweet.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import type { Tweet as ITweet, User as IUser } from "@prisma/client";
-import { Box, HStack, Icon, Text, Avatar } from "@chakra-ui/react";
-import { BsCartX, BsHeart } from "react-icons/bs";
+import {
+  Box,
+  HStack,
+  Icon,
+  Text,
+  Avatar,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { BsHeart } from "react-icons/bs";
 import { motion } from "framer-motion";
 import { trpc } from "../../utils/trpc";
 import { useSession } from "next-auth/react";
+import { TweetModal } from "./TweetModal";
 
 interface TweetProps {
   tweet: ITweet;
@@ -48,6 +56,8 @@ export const Tweet: React.FC<TweetProps> = ({
 
   const { data: session } = useSession();
 
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   const options = {
     weekday: "long",
     year: "numeric",
@@ -63,40 +73,51 @@ export const Tweet: React.FC<TweetProps> = ({
       width="xl"
       height="40"
       borderRadius="2xl"
+      cursor="pointer"
     >
-      <Box>
-        <Text>
-          <HStack>
-            <Avatar src={author.image as string} size="sm" />
-            <Text fontWeight="bold">{author.name}</Text>
-            <Text color="gray.400">
-              {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
-              {/* @ts-ignore */}
-              {tweet.createdAt.toLocaleDateString("en-US", options)}
-            </Text>
-          </HStack>
-        </Text>
-        <Box pt="2.5">
-          <Text>{tweet.message}</Text>
-        </Box>
-        <Box pt="10">
-          <Box boxSize="3xs">
-            <motion.div whileHover={{ scale: 1.01 }} whileTap={{ scaleY: 0.9 }}>
-              <Box cursor="pointer">
-                {likedBy.find((user) => user.id === session?.user?.id) ? (
-                  <HStack onClick={() => dislikeTweet({ tweetId: tweet.id })}>
-                    <Icon as={BsHeart} color="red.400" />
-                    <Text color="red.400">{likedBy.length}</Text>
-                  </HStack>
-                ) : (
-                  <HStack onClick={() => likeTweet({ tweetId: tweet.id })}>
-                    <Icon as={BsHeart} borderColor="red.400" />
-                    <Text>{likedBy.length}</Text>
-                  </HStack>
-                )}
-              </Box>
-            </motion.div>
+      <Box onClick={onOpen}>
+        <TweetModal
+          onClose={onClose}
+          onOpen={onOpen}
+          isOpen={isOpen}
+          likedBy={likedBy}
+          setTweets={setTweets}
+          tweet={tweet}
+        />
+        <Box>
+          <Text>
+            <HStack>
+              <Avatar src={author.image as string} size="sm" />
+              <Text fontWeight="bold">{author.name}</Text>
+              <Text color="gray.400">
+                {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+                {/* @ts-ignore */}
+                {tweet.createdAt.toLocaleDateString("en-US", options)}
+              </Text>
+            </HStack>
+          </Text>
+          <Box pt="2.5">
+            <Text>{tweet.message}</Text>
           </Box>
+        </Box>
+      </Box>
+      <Box pt="10">
+        <Box boxSize="3xs">
+          <motion.div whileHover={{ scale: 1.01 }} whileTap={{ scaleY: 0.9 }}>
+            <Box cursor="pointer">
+              {likedBy.find((user) => user.id === session?.user?.id) ? (
+                <HStack onClick={() => dislikeTweet({ tweetId: tweet.id })}>
+                  <Icon as={BsHeart} color="red.400" />
+                  <Text color="red.400">{likedBy.length}</Text>
+                </HStack>
+              ) : (
+                <HStack onClick={() => likeTweet({ tweetId: tweet.id })}>
+                  <Icon as={BsHeart} borderColor="red.400" />
+                  <Text>{likedBy.length}</Text>
+                </HStack>
+              )}
+            </Box>
+          </motion.div>
         </Box>
       </Box>
     </Box>
---
+++ src/components/tweet/TweetModal.tsx
@@ -0,0 +1,114 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  Button,
+  Text,
+  HStack,
+  Avatar,
+} from "@chakra-ui/react";
+import React from "react";
+import type { Tweet as ITweet, User as IUser } from "@prisma/client";
+import { useSession } from "next-auth/react";
+import { trpc } from "../../utils/trpc";
+
+interface TweetModalProps {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+  tweet: ITweet;
+  // author: IUser;
+  likedBy: IUser[];
+  setTweets: React.Dispatch<
+    React.SetStateAction<
+      (ITweet & {
+        author: IUser;
+        likedBy: IUser[];
+      })[]
+    >
+  >;
+}
+
+export const TweetModal: React.FC<TweetModalProps> = ({
+  isOpen,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  onOpen,
+  onClose,
+  likedBy,
+  setTweets,
+  tweet,
+}) => {
+  const { mutate: likeTweet } = trpc.tweet.likeTweet.useMutation({
+    onSuccess: (data) => {
+      setTweets((prev) => {
+        const index = prev.findIndex((t) => t.id === data.id);
+        prev[index] = data;
+        return [...prev];
+      });
+    },
+  });
+
+  const { mutate: dislikeTweet } = trpc.tweet.dislikeTweet.useMutation({
+    onSuccess: (data) => {
+      setTweets((prev) => {
+        const index = prev.findIndex((t) => t.id === data.id);
+        prev[index] = data;
+        return [...prev];
+      });
+    },
+  });
+
+  const { data: session } = useSession();
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Tweet Details</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Text>Liked By:</Text>
+          {likedBy.map((user) => (
+            <HStack key={user.id} pt="2">
+              <Avatar size="sm" src={user.image as string} />
+              <Text fontWeight="bold">{user.name}</Text>
+            </HStack>
+          ))}
+        </ModalBody>
+
+        <ModalFooter>
+          {likedBy.find((user) => user.id === session?.user?.id) ? (
+            <Button
+              colorScheme="twitter"
+              mr={3}
+              onClick={() => {
+                dislikeTweet({ tweetId: tweet.id });
+                onClose();
+              }}
+            >
+              Dislike
+            </Button>
+          ) : (
+            <Button
+              colorScheme="twitter"
+              mr={3}
+              onClick={() => {
+                likeTweet({ tweetId: tweet.id });
+                onClose();
+              }}
+            >
+              Like
+            </Button>
+          )}
+          <Button variant="ghost" colorScheme="twitter" onClick={onClose}>
+            Close
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
