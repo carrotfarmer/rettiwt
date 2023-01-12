56f886fe38b953e67ca54cdaf96809718332c4e0
Dhruva Srinivas <dhruvas17068@gmail.com>
2023-01-12 20:44:16 +0530

    fix: change page title in profile

--- src/pages/profile/[userId].tsx
+++ src/pages/profile/[userId].tsx
@@ -1,10 +1,12 @@
 import { Box, Center, Text } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import { useSession } from "next-auth/react";
+import Head from "next/head";
 import { useRouter } from "next/router";
 import React from "react";
 import { Navbar } from "../../components/nav/Navbar";
 import { Profile } from "../../components/user/Profile";
+import { trpc } from "../../utils/trpc";
 
 const UserProfile: NextPage = () => {
   const router = useRouter();
@@ -12,8 +14,18 @@ const UserProfile: NextPage = () => {
 
   const { data: session } = useSession();
 
+  const { data: user } = trpc.auth.getUser.useQuery({
+    id: userId as string,
+  });
+
   return (
     <div>
+      <Head>
+        <title>{user?.name} - rettiwt</title>
+        <meta name="description" content="ultra-mid twitter" />
+        <link rel="icon" href="/favicon-trans.png" />
+      </Head>
+
       <Navbar />
       {session ? (
         <Profile userId={userId as string} session={session} />
