d6fe1f8f4abe0e52881ace5118e2e08a0ad18390
Dhruva Srinivas <dhruvas17068@gmail.com>
2023-03-29 21:48:51 +0530

    feat: more meta tags

--- src/components/Metadata.tsx
+++ src/components/Metadata.tsx
@@ -17,6 +17,20 @@ export const Metadata: React.FC<MetadataProps> = ({ title, description }) => {
         <meta property="og:title" content={`${title} - rettiwt`} />
         <meta property="og:description" content={description} />
 
+        <meta
+          property="og:url"
+          content="https://rettiwtt.vercel.app/tweet/6ed381a0-458c-4df1-9f6a-e0445c5656dd"
+        />
+        <meta property="og:type" content="website" />
+
+        <meta property="twitter:domain" content="rettiwtt.vercel.app" />
+        <meta
+          property="twitter:url"
+          content="https://rettiwtt.vercel.app/tweet/6ed381a0-458c-4df1-9f6a-e0445c5656dd"
+        />
+        <meta name="twitter:title" content={`${title} - rettiwt`} />
+        <meta name="twitter:description" content={description} />
+
         {/* TODO: OG Image */}
       </Head>
     </div>
